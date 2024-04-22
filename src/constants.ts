3da7e85e920d76ad6e69eb0d24aea1089fd0ff3f
TheLazySquid <thelazysquid2@gmail.com>
2024-04-22 04:14:12 -0400

    Fix broken scaling

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const userAreaSelector = 'section[class*="panels__"]'
 export const serverListSelector = 'nav[class*="guilds__"]'
 export const containerSelector = `div:has(> ${serverListSelector})`
-export const layerSelector = `[class*="layers__"]`
+export const layerSelector = `[class*="baseLayer"]`
 export const channelsSelector = '[class*="sidebar_"] > nav'
 export const scaleRegex = /scale\((.*)\)/
 export const UAButtonsSelector = 'div[class*="avatarWrapper_"] + div'
